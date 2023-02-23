60e67f500cb1fbe9cd2ea97d4147c6a8e5f7d451
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2023-02-23 23:42:41 +0000

    Header Completed

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
+const burgermobile = document.querySelector('.offcanvasTogler1');
+const burger = document.querySelector('.offcanvasTogler2');
+const offcanvas = document.querySelector('.offcanvasHeader');
+const offcanvasOverlay = document.querySelector('.offcanvasHeaderOverlay');
 const navSlide = ()=> {
-    // Variables
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
     // Toggle Nav
-    burger.addEventListener('click' ,() => {
-        nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = '';            
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle')
+    burgermobile.addEventListener('click' ,() => {
+        offcanvas.classList.add('offcanvasHeaderActive');
+        offcanvasOverlay.classList.add('offcanvasHeaderActive');
+        console.log("ToglerMobile");
     });
+}
+const navSlide2 = ()=> {
+    burger.addEventListener('click' ,() => {
+        offcanvas.classList.add('offcanvasHeaderActive');
+        offcanvasOverlay.classList.add('offcanvasHeaderActive');
+        console.log("ToglerTablet");
+    });
+
+}
+
+const navClose = ()=> {
+    offcanvasOverlay.addEventListener('click' ,() => {
+    offcanvas.classList.remove('offcanvasHeaderActive');
+    offcanvasOverlay.classList.remove('offcanvasHeaderActive');
+    console.log("Close");
+});
+
 }
 
 navSlide();
+navSlide2();
+navClose();
