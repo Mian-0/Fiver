2432bffe3a5b95086fb5def8c5869f24662780aa
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2023-02-27 07:36:22 +0000

    Services Section Completed

--- app.js
+++ app.js
@@ -36,3 +36,33 @@ navClose();
 
 // Hero Section
 // Hero Section Completed
+
+// Services Section
+const slider = document.querySelector('.cardsSlider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+// Services Section Completed
