e91024be37a408ee9f496098475e17ee025df470
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2023-03-01 15:12:37 +0000

    Footer Completed

--- app.js
+++ app.js
@@ -131,3 +131,20 @@ function  movesliderProjects() {
   });
 }
 // Projects Section Completed
+
+// Footer
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+// Footer Completed
